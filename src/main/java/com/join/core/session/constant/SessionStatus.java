7ccb3d68bb4810123d1533dbe40c5a1289473db2
uuujini <youjin010402@naver.com>
2024-08-17 11:37:40 +0900

    Feat: `Session` 관련 도메인 및 클래스 추가

---
+++ src/main/java/com/join/core/session/constant/SessionStatus.java
@@ -0,0 +1,6 @@
+package com.join.core.session.constant;
+
+public enum SessionStatus {
+    WAITING, ACTIVE, COMPLETED, NOT_STARTED
+
+}
---
+++ src/main/java/com/join/core/session/domain/Session.java
@@ -0,0 +1,44 @@
+package com.join.core.session.domain;
+
+import com.join.core.session.constant.SessionStatus;
+import com.join.core.study.domain.Study;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private int sessionNumber;
+
+    @NotNull
+    private LocalDate studyDate;
+
+    @NotNull
+    private LocalTime stTime;
+
+    @NotNull
+    private LocalTime endTime;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private SessionStatus sessionStatus;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+
+}

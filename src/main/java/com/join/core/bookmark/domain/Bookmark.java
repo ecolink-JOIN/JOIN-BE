e46b53c069ad766141b3e9ddfff750a2dc9f4662
uuujini <youjin010402@naver.com>
2024-08-17 11:35:51 +0900

    Feat: `Bookmark` 도메인 추가

---
+++ src/main/java/com/join/core/bookmark/domain/Bookmark.java
@@ -0,0 +1,36 @@
+package com.join.core.bookmark.domain;
+
+import com.join.core.avatar.domain.Avatar;
+import com.join.core.common.domain.BaseTimeEntity;
+import com.join.core.study.domain.Study;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "avatar_id", nullable = false)
+    private Avatar avatar;
+
+    public Bookmark(Study study, Avatar avatar) {
+        this.study = study;
+        this.avatar = avatar;
+    }
+
+}

8026e92448c8ad6b374aac64e5c3d471483f2058
uuujini <youjin010402@naver.com>
2024-08-29 01:42:33 +0900

    Refactor: 기능 중복 코드 삭제

--- src/main/java/com/join/core/avatar/repository/AvatarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.join.core.avatar.repository;
-
-import com.join.core.avatar.domain.Avatar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-
-}

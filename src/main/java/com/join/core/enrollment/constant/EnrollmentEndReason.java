de1602ab0417772b6e166e0cc3ce4bbd1a977a9b
uuujini <youjin010402@naver.com>
2024-12-18 02:44:57 +0900

    Refactor: 가입 종료 사유 데이터 형식 수정

--- src/main/java/com/join/core/enrollment/constant/EnrollmentEndReason.java
+++
@@ -1,6 +0,0 @@
-package com.join.core.enrollment.constant;
-
-public enum EnrollmentEndReason {
-    ATTENDANCE, AUTHENTICATION, RATING, STUDY_COUNT, OTHER
-
-}
--- src/main/java/com/join/core/enrollment/domain/Enrollment.java
+++ src/main/java/com/join/core/enrollment/domain/Enrollment.java
@@ -1,13 +1,13 @@
 package com.join.core.enrollment.domain;
 
 import com.join.core.avatar.domain.Avatar;
-import com.join.core.enrollment.constant.EnrollmentEndReason;
 import com.join.core.enrollment.constant.EnrollmentStatus;
 import com.join.core.enrollment.constant.StudyRole;
 import com.join.core.study.domain.Study;
 import com.join.core.common.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +27,9 @@ public class Enrollment extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private EnrollmentStatus status;
 
+    @Size(min = 10, max = 150)
+    private String endReason;
+
     @NotNull
     @Enumerated(EnumType.STRING)
     private StudyRole role;
@@ -35,9 +38,6 @@ public class Enrollment extends BaseTimeEntity {
     private LocalDateTime enrolledDate;
 
     private LocalDateTime endDate;
-
-    @Enumerated(EnumType.STRING)
-    private EnrollmentEndReason endReason;
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
@@ -50,7 +50,7 @@ public class Enrollment extends BaseTimeEntity {
     private Avatar avatar;
 
     public Enrollment(Study study, Avatar avatar, EnrollmentStatus status, LocalDateTime enrolledDate,
-                      LocalDateTime endDate, EnrollmentEndReason endReason, StudyRole role) {
+                      LocalDateTime endDate, String endReason, StudyRole role) {
         this.study = study;
         this.avatar = avatar;
         this.status = status;

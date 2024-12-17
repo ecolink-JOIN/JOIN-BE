cacddd483635954c5a4fe8d249a1a5aed5c1ef6f
uuujini <youjin010402@naver.com>
2024-12-18 01:49:24 +0900

    Fix: 스터디 모집 날짜 검증 로직 추가

--- src/main/java/com/join/core/study/service/StudyRecruitService.java
+++ src/main/java/com/join/core/study/service/StudyRecruitService.java
@@ -7,6 +7,7 @@ import com.join.core.avatar.domain.AvatarReader;
 import com.join.core.category.domain.Category;
 import com.join.core.category.service.CategoryReader;
 import com.join.core.common.exception.ErrorCode;
+import com.join.core.common.exception.impl.InvalidParamException;
 import com.join.core.common.exception.impl.NoPermissionException;
 import com.join.core.schedule.domain.StudySchedule;
 import com.join.core.study.domain.Study;
@@ -20,8 +21,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.join.core.common.exception.ErrorCode.INVALID_PARAMETER;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +40,15 @@ public class StudyRecruitService {
 
     @Transactional
     public void createStudy(Long avatarId, StudyRecruitRequest recruitRequest) {
+        LocalDate now = LocalDate.now();
+
+        if (recruitRequest.getStDate().isBefore(now) || recruitRequest.getRecruitEndDate().isBefore(now)) {
+            throw new InvalidParamException(INVALID_PARAMETER, "스터디 시작일과 모집 종료일은 현재 이후여야 합니다.");
+        }
+        if (recruitRequest.getStDate().isAfter(recruitRequest.getEndDate())) {
+            throw new InvalidParamException(INVALID_PARAMETER, "시작일보다 종료일이 이후여야 합니다.");
+        }
+
         Avatar writer = avatarReader.getAvatarById(avatarId);
         Address address = addressReader.getAddressByLocation(recruitRequest.getProvince(), recruitRequest.getCity());
         Category category = categoryReader.getCategoryByName(recruitRequest.getCategoryName());
@@ -69,9 +82,14 @@ public class StudyRecruitService {
         if (!study.getWriter().getId().equals(avatarId)) {
             throw new NoPermissionException(ErrorCode.UNAUTHORIZED_ACCESS);
         }
+        if (reRecruitRequest.getRecruitEndDate().isBefore(LocalDate.now())) {
+            throw new InvalidParamException(INVALID_PARAMETER, "모집 종료일은 현재 이후여야 합니다.");
+        }
+        if (study.getStDate().isAfter(reRecruitRequest.getRecruitEndDate())) {
+            throw new InvalidParamException(INVALID_PARAMETER, "시작일보다 종료일이 이후여야 합니다.");
+        }
 
         study.updateRecruitDetails(reRecruitRequest);
-
         studyStore.store(study);
     }
 

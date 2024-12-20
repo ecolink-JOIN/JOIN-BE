759ad59a74382155753e0cd5f5e3707b7cc3364e
moveuk <v_donguk@naver.com>
2024-12-21 01:46:56 +0900

    Feat: 스터디를 통한 회차 리스트 조회 추가

---
+++ src/main/java/com/join/core/meeting/repository/MeetingReaderImpl.java
@@ -0,0 +1,21 @@
+package com.join.core.meeting.repository;
+
+import com.join.core.meeting.domain.Meeting;
+import com.join.core.meeting.service.MeetingReader;
+import com.join.core.study.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class MeetingReaderImpl implements MeetingReader {
+
+    private final MeetingRepository meetingRepository;
+
+    @Override
+    public List<Meeting> findByStudy(Study study) {
+        return meetingRepository.findByStudy(study);
+    }
+}
---
+++ src/main/java/com/join/core/meeting/service/MeetingReader.java
@@ -0,0 +1,11 @@
+package com.join.core.meeting.service;
+
+import com.join.core.meeting.domain.Meeting;
+import com.join.core.study.domain.Study;
+
+import java.util.List;
+
+public interface MeetingReader {
+    List<Meeting> findByStudy(Study study);
+
+}

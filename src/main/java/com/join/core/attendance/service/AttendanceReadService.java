c80c0d2f93d0106a03a60186fd2b151e19e47559
김연진 <love_homebody@naver.com>
2024-12-22 19:30:03 +0900

    feat: #53 스터디 참여자가 아닌 사용자가 출석 내역 기록시 예외 발생

--- src/main/java/com/join/core/attendance/service/AttendanceReadService.java
+++ src/main/java/com/join/core/attendance/service/AttendanceReadService.java
@@ -5,6 +5,9 @@ import com.join.core.attendance.dto.response.CheckAttendanceResponse;
 import com.join.core.attendance.service.command.CheckAttendanceCommand;
 import com.join.core.avatar.domain.Avatar;
 import com.join.core.avatar.domain.AvatarReader;
+import com.join.core.common.exception.ErrorCode;
+import com.join.core.common.exception.impl.NoPermissionException;
+import com.join.core.enrollment.service.EnrollmentReader;
 import com.join.core.meeting.domain.Meeting;
 import com.join.core.meeting.service.MeetingReader;
 import com.join.core.study.domain.Study;
@@ -20,13 +23,22 @@ public class AttendanceReadService {
     private final MeetingReader meetingReader;
     private final StudyReader studyReader;
     private final AvatarReader avatarReader;
+    private final EnrollmentReader enrollmentReader;
 
     public CheckAttendanceResponse checkAttendance(CheckAttendanceCommand command) {
         Avatar avatar = avatarReader.getAvatarById(command.avatarId());
         Study study = studyReader.getStudyByToken(command.studyToken());
+        checkMember(avatar.getId(), study.getId());
+
         Meeting meeting = meetingReader.findByStudyIdAndMeetingNo(study.getId(), command.meetingNo());
 
         return findAttendance(avatar.getId(), meeting.getId());
+    }
+
+    private void checkMember(Long avatarId, Long studyId) {
+        if (!enrollmentReader.existEnrollmentByAvatarIdAndStudyId(avatarId, studyId)) {
+            throw new NoPermissionException(ErrorCode.UNAUTHORIZED_ACCESS);
+        }
     }
 
     private CheckAttendanceResponse findAttendance(Long avatarId, Long meetingId) {

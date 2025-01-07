6dae494f673dc966a25f0bc9df74feab1a49f11a
김연진 <love_homebody@naver.com>
2025-01-07 17:24:30 +0900

    feat: 스터디 참여자가 아닌 사용자가 접근할 경우 예외 발생

--- src/main/java/com/join/core/proof/service/ProofReadService.java
+++ src/main/java/com/join/core/proof/service/ProofReadService.java
@@ -2,6 +2,9 @@ package com.join.core.proof.service;
 
 import com.join.core.avatar.domain.Avatar;
 import com.join.core.avatar.domain.AvatarReader;
+import com.join.core.common.exception.ErrorCode;
+import com.join.core.common.exception.impl.BadRequestException;
+import com.join.core.enrollment.service.EnrollmentReader;
 import com.join.core.meeting.domain.Meeting;
 import com.join.core.meeting.domain.MeetingReader;
 import com.join.core.proof.domain.Proof;
@@ -21,15 +24,25 @@ public class ProofReadService {
     private final ProofReader proofReader;
     private final AvatarReader avatarReader;
     private final StudyReader studyReader;
+    private final EnrollmentReader enrollmentReader;
     private final MeetingReader meetingReader;
 
     @Transactional
     public CheckProofResponse getProofStatus(CheckProofCommand command) {
         Avatar avatar = avatarReader.getAvatarById(command.avatarId());
         Study study = studyReader.getStudyByToken(command.studyToken());
+        checkAuthorization(avatar.getId(), study.getId());
+
         Meeting meeting = meetingReader.findByStudyIdAndMeetingNo(study.getId(), command.meetingNo());
         Proof proof = proofReader.findLastProof(avatar.getId(), meeting.getId());
+
         return getProofResponse(proof);
+    }
+
+    private void checkAuthorization(Long avatarId, Long studyId) {
+        if (!enrollmentReader.existEnrollmentByAvatarIdAndStudyId(avatarId, studyId)) {
+            throw new BadRequestException(ErrorCode.NOT_MEMBER_OF_STUDY);
+        }
     }
 
     private CheckProofResponse getProofResponse(Proof proof) {

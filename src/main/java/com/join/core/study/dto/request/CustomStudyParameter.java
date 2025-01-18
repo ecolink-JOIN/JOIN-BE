85b1592e4d81805f7e080836fd96819f479bef53
김연진 <love_homebody@naver.com>
2025-01-18 19:24:32 +0900

    feat: swagger 리퀘스트 예시 추가

--- src/main/java/com/join/core/study/dto/request/CustomStudyParameter.java
+++ src/main/java/com/join/core/study/dto/request/CustomStudyParameter.java
@@ -3,18 +3,19 @@ package com.join.core.study.dto.request;
 import com.join.core.common.constant.DayType;
 import com.join.core.study.constant.StudyForm;
 import com.join.core.study.constant.TimeZone;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record CustomStudyParameter(
-        String category,
-        StudyForm form,
+        @Schema(example = "입시") String category,
+        @Schema(example = "ONLINE") StudyForm form,
         List<DayType> possibleDays,
-        TimeZone timeZone,
-        Integer minParticipationCount,
-        Integer maxParticipationCount,
-        String province,
-        String city
+        @Schema(example = "MORNING", type = "list") TimeZone timeZone,
+        @Schema(example = "0") Integer minParticipationCount,
+        @Schema(example = "7") Integer maxParticipationCount,
+        @Schema(example = "서울특별시") String province,
+        @Schema(example = "도봉구") String city
 ) {
 
     public CustomStudyParameter {
--- src/main/java/com/join/core/study/dto/request/SearchParameter.java
+++ src/main/java/com/join/core/study/dto/request/SearchParameter.java
@@ -1,4 +1,6 @@
 package com.join.core.study.dto.request;
 
-public record SearchParameter(String keyword) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record SearchParameter(@Schema(example = "검색할 스터디의 제목") String keyword) {
 }

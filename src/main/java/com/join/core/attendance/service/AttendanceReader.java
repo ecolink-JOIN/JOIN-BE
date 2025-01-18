0ca5f9e6ec369283c0a428e9aef4548ec75ad1b0
moveuk <v_donguk@naver.com>
2025-01-19 01:03:34 +0900

    feat: 출석율 계산 추가

--- src/main/java/com/join/core/attendance/service/AttendanceReader.java
+++ src/main/java/com/join/core/attendance/service/AttendanceReader.java
@@ -2,6 +2,7 @@ package com.join.core.attendance.service;
 
 import com.join.core.attendance.domain.Attendance;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceReader {
@@ -9,4 +10,8 @@ public interface AttendanceReader {
     boolean existsAttendance(Long avatarId, Long meetingId);
 
     Optional<Attendance> findAttendance(Long avatarId, Long meetingId);
+
+    List<Attendance> findAttendanceForJoinedStudy(Long avatarId);
+
+    List<Attendance> findAttendanceForLeftStudy(Long avatarId);
 }

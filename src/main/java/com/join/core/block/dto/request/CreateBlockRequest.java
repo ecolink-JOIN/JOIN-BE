79f7a891424d041e016fa3ad4ba8357240700a99
김연진 <love_homebody@naver.com>
2025-01-20 17:42:00 +0900

    feat: swagger 작성

--- src/main/java/com/join/core/block/dto/request/CreateBlockRequest.java
+++ src/main/java/com/join/core/block/dto/request/CreateBlockRequest.java
@@ -1,6 +1,13 @@
 package com.join.core.block.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
-public record CreateBlockRequest(String targetAvatarToken, LocalDate blockDate) {
+@Schema
+public record CreateBlockRequest(
+        @NotNull @Schema(example = "avt_token") String targetAvatarToken,
+        @NotNull @Schema(example = "2025-01-20") LocalDate blockDate
+) {
 }

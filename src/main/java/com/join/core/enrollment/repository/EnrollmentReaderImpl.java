99656247e4683e7e34bfef71d1e232437f86e2c3
uuujini <youjin010402@naver.com>
2025-01-29 01:10:47 +0900

    Refactor: 평가대상자 기준 변경

--- src/main/java/com/join/core/enrollment/repository/EnrollmentReaderImpl.java
+++ src/main/java/com/join/core/enrollment/repository/EnrollmentReaderImpl.java
@@ -32,9 +32,9 @@ public class EnrollmentReaderImpl implements EnrollmentReader {
 
     @Override
     public void validateEnrollment(Long avatarId, Long studyId) {
-        boolean exists = enrollmentRepository.existsByAvatarIdAndStudyIdAndStatus(avatarId, studyId, EnrollmentStatus.JOINED);
+        boolean exists = enrollmentRepository.existsByAvatarIdAndStudyIdAndStatusNot(avatarId, studyId, EnrollmentStatus.PENDING);
         if (!exists) {
-            throw new InvalidParamException(ErrorCode.INVALID_PARAMETER, "스터디에 참여하지 않은 아바타입니다.");
+            throw new InvalidParamException(ErrorCode.INVALID_PARAMETER, "스터디 참여자가 아닙니다.");
         }
     }
 

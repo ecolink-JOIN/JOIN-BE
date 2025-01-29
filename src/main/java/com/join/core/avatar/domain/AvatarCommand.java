206d809770dce89d25cb94c00e61ed16a4345124
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2025-01-29 21:17:33 +0900

    Fix: 바인딩 오류 해결

--- src/main/java/com/join/core/avatar/domain/AvatarCommand.java
+++ src/main/java/com/join/core/avatar/domain/AvatarCommand.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class AvatarCommand {
 
@@ -11,6 +12,7 @@ public class AvatarCommand {
 	}
 
 	@Getter
+	@Setter
 	@NoArgsConstructor
 	@AllArgsConstructor
 	public static class ChangeNickname {

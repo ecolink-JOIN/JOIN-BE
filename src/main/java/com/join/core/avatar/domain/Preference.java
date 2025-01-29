a6f508532d39ea80826129c1bec6d4ffcfa3ae25
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2025-01-29 21:15:29 +0900

    Fix: 컴파일 오류 해결

--- src/main/java/com/join/core/avatar/domain/Preference.java
+++ src/main/java/com/join/core/avatar/domain/Preference.java
@@ -6,12 +6,16 @@ import com.join.core.common.constant.DayType;
 import com.join.core.study.constant.StudyForm;
 import com.join.core.study.constant.TimeZone;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamoDbBean
 public class Preference {
 

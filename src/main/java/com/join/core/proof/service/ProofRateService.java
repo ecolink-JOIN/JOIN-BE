4b15039d2a328e7908b1cbc008c3369e2a99711d
moveuk <v_donguk@naver.com>
2025-02-01 02:31:58 +0900

    feat: 스터디 인증율(팀, 개인) 계산 메서드 추가

--- src/main/java/com/join/core/proof/service/ProofRateService.java
+++ src/main/java/com/join/core/proof/service/ProofRateService.java
@@ -1,5 +1,6 @@
 package com.join.core.proof.service;
 
+import com.join.core.avatar.domain.AvatarReader;
 import com.join.core.common.util.NumberUtil;
 import com.join.core.enrollment.constant.EnrollmentStatus;
 import com.join.core.meeting.domain.MeetingReader;
@@ -15,6 +16,7 @@ import java.util.stream.Stream;
 @RequiredArgsConstructor
 public class ProofRateService {
 
+    private final AvatarReader avatarReader;
     private final MeetingReader meetingReader;
     private final ProofReader proofReader;
 
@@ -46,5 +48,62 @@ public class ProofRateService {
 
         return NumberUtil.round(2, totalProof / totalMeetings * 100);
     }
+
+    @Transactional(readOnly = true)
+    public double calculateTeamAttendanceRateForStudy(Long studyId) {
+        List<Proof> attendancesForJoinedStudies = proofReader.findByStudyIdForJoinedStudy(studyId);
+        List<Proof> attendancesForLeftStudies = proofReader.findByStudyIdForLeftStudy(studyId);
+
+        long countStudyMembers = avatarReader.findAvatarsExceptPendingByStudyId(studyId).size();
+        long countMeetings = meetingReader.findMeetingsByStudyId(studyId).size();
+        long totalMeetings = countMeetings * countStudyMembers;
+
+        double totalAttendance = Stream.concat(
+                        attendancesForJoinedStudies.stream(),
+                        attendancesForLeftStudies.stream()
+                )
+                .mapToDouble(attendance -> {
+                    double reflectionRate = attendance.getStatus().getReflectionRate();
+
+                    if (attendancesForLeftStudies.contains(attendance)) {
+                        reflectionRate *= EnrollmentStatus.LEFT.getReflectionRate();
+                    }
+
+                    return reflectionRate;
+                })
+                .sum();
+
+        if (totalAttendance == 0) return 0.0;
+
+        return NumberUtil.round(2, totalAttendance / totalMeetings * 100);
+    }
+
+    @Transactional(readOnly = true)
+    public double calculateMembersAttendanceRateForStudy(Long avatarId, Long studyId) {
+        List<Proof> proofsForJoinedStudies = proofReader.findByAvatarIdAndStudyIdForJoinedStudy(avatarId, studyId);
+        List<Proof> proofsForLeftStudies = proofReader.findByAvatarIdAndStudyIdForLeftStudy(avatarId, studyId);
+
+        long totalMeetings = meetingReader.findMeetingsByStudyId(studyId).size();
+
+        double totalProof = Stream.concat(
+                        proofsForJoinedStudies.stream(),
+                        proofsForLeftStudies.stream()
+                )
+                .mapToDouble(proof -> {
+                    double reflectionRate = proof.getStatus().getReflectionRate();
+
+                    if (proofsForLeftStudies.contains(proof)) {
+                        reflectionRate *= EnrollmentStatus.LEFT.getReflectionRate();
+                    }
+
+                    return reflectionRate;
+                })
+                .sum();
+
+        if (totalProof == 0) {
+            return 0.0;
+        }
+
+        return NumberUtil.round(2, totalProof / totalMeetings * 100);
     }
 }

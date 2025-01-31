620e51c2a83517b58587ce907379d8170eef5b4c
moveuk <v_donguk@naver.com>
2025-02-01 02:11:26 +0900

    feat: study kakao 링크 url 컬럼 추가

--- src/main/java/com/join/core/attendance/service/AttendanceRateService.java
+++ src/main/java/com/join/core/attendance/service/AttendanceRateService.java
@@ -1,6 +1,7 @@
 package com.join.core.attendance.service;
 
 import com.join.core.attendance.domain.Attendance;
+import com.join.core.avatar.domain.AvatarReader;
 import com.join.core.common.util.NumberUtil;
 import com.join.core.enrollment.constant.EnrollmentStatus;
 import com.join.core.meeting.domain.MeetingReader;
@@ -44,5 +45,60 @@ public class AttendanceRateService {
 
         return NumberUtil.round(2, totalAttendance / totalMeetings * 100);
     }
+
+    @Transactional(readOnly = true)
+    public double calculateTeamAttendanceRateForStudy(Long studyId) {
+        List<Attendance> attendancesForJoinedStudies = attendanceReader.findByStudyIdForJoinedStudy(studyId);
+        List<Attendance> attendancesForLeftStudies = attendanceReader.findByStudyIdForLeftStudy(studyId);
+
+        long countStudyMembers = avatarReader.findAvatarsExceptLeftByStudyId(studyId).size();
+        long countAllMeetings = meetingReader.findMeetingsByStudyId(studyId).size();
+        long totalMeetings = countAllMeetings * countStudyMembers;
+
+        double totalAttendance = Stream.concat(
+                        attendancesForJoinedStudies.stream(),
+                        attendancesForLeftStudies.stream()
+                )
+                .mapToDouble(attendance -> {
+                    double reflectionRate = attendance.getStatus().getReflectionRate();
+
+                    if (attendancesForLeftStudies.contains(attendance)) {
+                        reflectionRate *= EnrollmentStatus.LEFT.getReflectionRate();
+                    }
+
+                    return reflectionRate;
+                })
+                .sum();
+
+        if (totalAttendance == 0) return 0.0;
+
+        return NumberUtil.round(2, totalAttendance / totalMeetings * 100);
+    }
+
+    @Transactional(readOnly = true)
+    public double calculateMemberAttendanceRateForStudy(Long avatarId, Long studyId) {
+        List<Attendance> attendancesForJoinedStudies = attendanceReader.findByAvatarIdAndStudyIdForJoinedStudy(avatarId, studyId);
+        List<Attendance> attendancesForLeftStudies = attendanceReader.findByAvatarIdAndStudyIdForLeftStudy(avatarId, studyId);
+
+        long totalMeetings = meetingReader.findMeetingsByStudyId(studyId).size();
+
+        double totalAttendance = Stream.concat(
+                        attendancesForJoinedStudies.stream(),
+                        attendancesForLeftStudies.stream()
+                )
+                .mapToDouble(attendance -> {
+                    double reflectionRate = attendance.getStatus().getReflectionRate();
+
+                    if (attendancesForLeftStudies.contains(attendance)) {
+                        reflectionRate *= EnrollmentStatus.LEFT.getReflectionRate();
+                    }
+
+                    return reflectionRate;
+                })
+                .sum();
+
+        if (totalAttendance == 0) return 0.0;
+
+        return NumberUtil.round(2, totalAttendance / totalMeetings * 100);
     }
 }

8009469871c0df544547249a371358657402f6e9
moveuk <v_donguk@naver.com>
2025-02-01 02:29:41 +0900

    feat: 인증 리스트 조회 메서드 추가
    pending을 제외한 가입 및 탈퇴 유저 조회 분리
    - 스터디 전체 인증 리스트 조회 추가
    - 스터디에서 특정 유저의 인증 리스트 조회 추가
    - 스터디에서 특정 유저의 인증 확인이 필요한지에 대한 여부 조회 추가

--- src/main/java/com/join/core/proof/repository/ProofQueryRepositoryImpl.java
+++ src/main/java/com/join/core/proof/repository/ProofQueryRepositoryImpl.java
@@ -1,17 +1,20 @@
 package com.join.core.proof.repository;
 
 import com.join.core.enrollment.constant.EnrollmentStatus;
-import com.join.core.enrollment.domain.QEnrollment;
-import com.join.core.meeting.domain.QMeeting;
+import com.join.core.proof.constant.ProofStatus;
 import com.join.core.proof.domain.Proof;
-import com.join.core.proof.domain.QProof;
-import com.join.core.study.domain.QStudy;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+import static com.join.core.avatar.domain.QAvatar.*;
+import static com.join.core.enrollment.domain.QEnrollment.*;
+import static com.join.core.meeting.domain.QMeeting.*;
+import static com.join.core.proof.domain.QProof.*;
+import static com.join.core.study.domain.QStudy.*;
 
 @RequiredArgsConstructor
 @Component
@@ -21,27 +24,70 @@ public class ProofQueryRepositoryImpl implements ProofQueryRepository {
 
     @Override
     public List<Proof> findProofsByAvatarIdAndEnrollmentStatus(Long avatarId, List<EnrollmentStatus> statuses) {
-        QProof proof = QProof.proof;
-        QMeeting meeting = QMeeting.meeting;
-        QStudy study = QStudy.study;
-        QEnrollment enrollment = QEnrollment.enrollment;
-
         return queryFactory
                 .selectFrom(proof)
                 .distinct()
                 .join(proof.meeting, meeting)
                 .join(meeting.study, study)
                 .where(
-                        study.id.in(
-                                JPAExpressions
-                                        .select(enrollment.study.id)
-                                        .from(enrollment)
-                                        .where(
-                                                enrollment.avatar.id.eq(avatarId)
-                                                        .and(enrollment.status.in(statuses))
-                                        )
-                        )
+                        JPAExpressions
+                                .selectOne()
+                                .from(enrollment)
+                                .where(
+                                        enrollment.study.id.eq(study.id)
+                                                .and(enrollment.avatar.id.eq(avatarId))
+                                                .and(enrollment.status.in(statuses))
+                                )
+                                .exists(),
+                        proof.avatar.id.eq(avatarId)
                 )
                 .fetch();
+    }
+
+    @Override
+    public List<Proof> findProofsByStudyIdInEnrollmentStatuses(Long studyId, List<EnrollmentStatus> statuses) {
+        return queryFactory
+                .selectFrom(proof)
+                .join(proof.meeting, meeting)
+                .join(proof.avatar, avatar)
+                .join(meeting.study, study).on(study.id.eq(studyId))
+                .leftJoin(enrollment).on(
+                        enrollment.study.eq(study)
+                                .and(enrollment.avatar.eq(avatar))
+                )
+                .where(
+                        enrollment.status.in(statuses)
+                )
+                .fetch();
+    }
+
+    @Override
+    public List<Proof> findByAvatarIdAndStudyIdInEnrollmentStatuses(Long avatarId, Long studyId, List<EnrollmentStatus> statuses) {
+        return queryFactory
+                .selectFrom(proof)
+                .join(proof.avatar, avatar).on(proof.avatar.id.eq(avatarId))
+                .join(proof.meeting, meeting)
+                .join(meeting.study, study).on(study.id.eq(studyId))
+                .leftJoin(enrollment).on(
+                        enrollment.study.eq(study)
+                                .and(enrollment.avatar.eq(avatar)))
+                .where(enrollment.status.in(statuses))
+                .fetch();
+    }
+
+    @Override
+    public boolean allProofsHaveApprovedStatus(Long avatarId, Long studyId) {
+        Long countNonApproved = queryFactory
+                .select(proof.count())
+                .from(proof)
+                .join(avatar).on(proof.avatar.id.eq(avatarId))
+                .join(meeting).on(proof.meeting.id.eq(meeting.id))
+                .join(study).on(study.id.eq(studyId))
+                .where(
+                        proof.status.ne(ProofStatus.APPROVED)
+                )
+                .fetchOne();
+
+        return countNonApproved == null || countNonApproved == 0;
     }
 }

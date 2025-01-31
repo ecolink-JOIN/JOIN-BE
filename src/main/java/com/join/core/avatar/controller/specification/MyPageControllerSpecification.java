7da243ba3576f119607050da2866f8c58745b0fe
moveuk <v_donguk@naver.com>
2025-02-01 02:33:10 +0900

    feat: 운영중인 스터디 조회 컨트롤러 api 및 명세 추가

--- src/main/java/com/join/core/avatar/controller/specification/MyPageControllerSpecification.java
+++ src/main/java/com/join/core/avatar/controller/specification/MyPageControllerSpecification.java
@@ -1,12 +1,15 @@
 package com.join.core.avatar.controller.specification;
 
 import com.join.core.auth.domain.UserPrincipal;
+import com.join.core.avatar.dto.response.MyManagedStudyInfoResponse;
 import com.join.core.avatar.dto.response.MyPageInfoResponse;
 import com.join.core.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.util.List;
 
 public interface MyPageControllerSpecification {
 
@@ -15,5 +18,12 @@ public interface MyPageControllerSpecification {
             description = "마이페이지 조회",
             security = {@SecurityRequirement(name = "session-token")})
     ApiResponse<MyPageInfoResponse> getMyPageInfo(
+            @AuthenticationPrincipal UserPrincipal principal);
+
+    @Tag(name = "${swagger.tag.user}")
+    @Operation(summary = "마이페이지 운영중인 스터디 목록 조회 - 인증 필수",
+            description = "마이페이지 운영중인 스터디 목록을 조회해옵니다.",
+            security = {@SecurityRequirement(name = "session-token")})
+    ApiResponse<List<MyManagedStudyInfoResponse>> getMyManagedStudies(
             @AuthenticationPrincipal UserPrincipal principal);
 }

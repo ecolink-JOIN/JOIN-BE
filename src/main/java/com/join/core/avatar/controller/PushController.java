1235ba16d9665bde0d64618db4b6f973d89a367c
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2025-02-04 21:31:07 +0900

    Feat: 명세 목적의 인터페이스 분리

--- src/main/java/com/join/core/avatar/controller/PushController.java
+++ src/main/java/com/join/core/avatar/controller/PushController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.join.core.auth.domain.UserPrincipal;
+import com.join.core.avatar.controller.specification.PushControllerSpecification;
 import com.join.core.avatar.domain.AvatarCommand;
 import com.join.core.avatar.service.PushService;
 import com.join.core.common.response.ApiResponse;
@@ -20,15 +21,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/avatars/push")
-public class PushController {
+public class PushController implements PushControllerSpecification {
 
 	private final PushService pushService;
 
-	@Tag(name = "${swagger.tag.sign-up}")
-	@Tag(name = "${swagger.tag.user}")
-	@Operation(summary = "푸시 알림 동의 API - 인증 필요",
-		description = "푸시 알림 동의 API - 인증 필요",
-		security = {@SecurityRequirement(name = "session-token")})
+
 	@PreAuthorize("isAuthenticated()")
 	@PutMapping
 	public ApiResponse<Void> changePushConsent(@AuthenticationPrincipal UserPrincipal principal,

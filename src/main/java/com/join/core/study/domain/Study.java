e0611980bc3913c7b08688ab41fbab303000ea77
uuujini <youjin010402@naver.com>
2025-02-26 23:09:48 +0900

    Refactor: 모집인원 필드 null 허용으로 변경

--- src/main/java/com/join/core/study/domain/Study.java
+++ src/main/java/com/join/core/study/domain/Study.java
@@ -13,17 +13,7 @@ import com.join.core.study.constant.StudyForm;
 import com.join.core.study.constant.StudyStatus;
 import com.join.core.study.dto.request.StudyReRecruitRequest;
 import com.join.core.study.dto.request.StudyRecruitRequest;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -61,8 +51,8 @@ public class Study {
     @NotNull
     private String content;
 
-    @NotNull
-    private int capacity;
+    @Column(nullable = true)
+    private Integer capacity;
 
     @NotNull
     private String ruleExp;

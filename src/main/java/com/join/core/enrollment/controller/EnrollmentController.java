22311e2d13ed0f7e7dbb302c97e9a28f07854a11
김연진 <love_homebody@naver.com>
2025-03-17 17:18:40 +0900

    feat: swagger 작성

--- src/main/java/com/join/core/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/join/core/enrollment/controller/EnrollmentController.java
@@ -2,14 +2,15 @@ package com.join.core.enrollment.controller;
 
 import com.join.core.auth.domain.UserPrincipal;
 import com.join.core.common.response.ApiResponse;
+import com.join.core.enrollment.controller.specification.EnrollmentControllerSpecification;
 import com.join.core.enrollment.dto.request.DelegateLeaderRequest;
 import com.join.core.enrollment.service.EnrollmentService;
 import com.join.core.enrollment.service.dto.DelegateLeaderParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/study/{studyToken}/enrollments")
-public class EnrollmentController {
+public class EnrollmentController implements EnrollmentControllerSpecification {
 
     private final EnrollmentService enrollmentService;
 
     @PreAuthorize("isAuthenticated()")
-    @PostMapping("/delegate")
+    @PatchMapping("/delegate")
     public ApiResponse<Void> delegateStudyLeader(
             @AuthenticationPrincipal UserPrincipal principal,
             @PathVariable String studyToken,

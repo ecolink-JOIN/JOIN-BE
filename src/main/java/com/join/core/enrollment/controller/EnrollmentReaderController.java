1acbfcdb672ec22ea8b80bde3cc57e74e27fcd28
김연진 <love_homebody@naver.com>
2025-03-30 17:30:19 +0900

    feat: 스터디원 별 참여 현황 swagger 추가

--- src/main/java/com/join/core/enrollment/controller/EnrollmentReaderController.java
+++ src/main/java/com/join/core/enrollment/controller/EnrollmentReaderController.java
@@ -2,10 +2,12 @@ package com.join.core.enrollment.controller;
 
 import com.join.core.auth.domain.UserPrincipal;
 import com.join.core.common.response.ApiResponse;
+import com.join.core.enrollment.controller.specification.EnrollmentReaderControllerSpecification;
 import com.join.core.enrollment.dto.response.ProofAndAttendanceStatusResponse;
 import com.join.core.enrollment.service.EnrollmentReadService;
 import com.join.core.enrollment.service.dto.ParticipationDetailsParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/study/{studyToken}/enrollments")
-public class EnrollmentReaderController {
+public class EnrollmentReaderController implements EnrollmentReaderControllerSpecification {
 
     private final EnrollmentReadService enrollmentReadService;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{targetToken}")
     public ApiResponse<ProofAndAttendanceStatusResponse> getParticipationDetails(
             @AuthenticationPrincipal UserPrincipal principal,

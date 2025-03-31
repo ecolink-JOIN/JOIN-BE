cf30ecb907189ee70e3a278c3ff2632226707a0a
uuujini <youjin010402@naver.com>
2025-03-31 19:23:32 +0900

    Feat: 스터디 탈퇴 요청 조회 API 추가

--- src/main/java/com/join/core/withdraw/repository/WithdrawRepository.java
+++ src/main/java/com/join/core/withdraw/repository/WithdrawRepository.java
@@ -2,10 +2,15 @@ package com.join.core.withdraw.repository;
 
 import com.join.core.study.domain.Study;
 import com.join.core.avatar.domain.Avatar;
+import com.join.core.withdraw.constant.WithdrawStatus;
+import com.join.core.withdraw.constant.WithdrawType;
 import com.join.core.withdraw.domain.Withdraw;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WithdrawRepository extends JpaRepository<Withdraw, Long> {
     boolean existsByAvatarAndStudy(Avatar avatar, Study study);
+    List<Withdraw> findByStatusAndWithdrawType(WithdrawStatus withdrawStatus, WithdrawType withdrawType);
 
 }

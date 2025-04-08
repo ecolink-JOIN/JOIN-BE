4a396dd9fff398b05c5aa7049703ed3b71234008
uuujini <youjin010402@naver.com>
2025-04-08 18:14:13 +0900

    Fix: import문 오류 수정

--- src/main/java/com/join/core/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/join/core/enrollment/repository/EnrollmentRepository.java
@@ -2,6 +2,10 @@ package com.join.core.enrollment.repository;
 
 import com.join.core.enrollment.constant.EnrollmentStatus;
 import com.join.core.enrollment.domain.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
